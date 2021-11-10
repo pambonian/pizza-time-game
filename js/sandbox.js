d43729b88c15897208000b08d4a1619ca885a532
pambonian <pambonian@gmail.com>
2021-11-09 19:57:07 -0500

    sprites added

--- js/sandbox.js
+++ js/sandbox.js
@@ -2,69 +2,89 @@ function draw() {
     let canvas = document.getElementById('game');
     if (canvas.getContext) {
         let ctx = canvas.getContext('2d');
-        let taxii;
-        let player;
+        
+        // //red cube
+        // ctx.fillStyle = 'rgb(200, 0, 0)';
+        // ctx.fillRect(10, 10, 50, 50);
 
-        //red cube
-        ctx.fillStyle = 'rgb(200, 0, 0)';
-        ctx.fillRect(10, 10, 50, 50);
+        // //purple cube
+        // ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+        // ctx.fillRect(40, 70, 50, 50);
 
-        //purple cube
-        ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
-        ctx.fillRect(40, 70, 50, 50);
+        // //circle
+        // let circle = new Path2D();
+        // circle.arc(100, 35, 25, 0, 2 * Math.PI);
+        // ctx.stroke(circle);
+        // ctx.fill(circle);
 
-        //circle
-        let circle = new Path2D();
-        circle.arc(100, 35, 25, 0, 2 * Math.PI);
-        ctx.stroke(circle);
-        ctx.fill(circle);
+        // //path
+        // ctx.beginPath();
+        // ctx.strokeStyle = 'black';
+        // ctx.moveTo(0, 320);
+        // ctx.lineTo(700, 320);
+        // ctx.stroke();
 
-        //path
-        ctx.beginPath();
-        ctx.strokeStyle = 'black';
-        ctx.moveTo(0, 320);
-        ctx.lineTo(700, 320);
-        ctx.stroke();
-
-        //path 2
-        ctx.beginPath();
-        ctx.strokeStyle = 'black';
-        ctx.moveTo(0, 250);
-        ctx.lineTo(700, 250);
-        ctx.stroke();
-
-        //constructor class
-        class Crawler {
-            constructor(x, y, img, width, height) {
-                this.x = x;
-                this.y = y;
-                this.img = './assets/taxi.png';
-                this.width = width;
-                this.height = height;
-                this.alive = true;
-                this.render = function () {
-                    ctx.drawImage(img, 50, 50);
-                }
-            }
-        };
+        // //path 2
+        // ctx.beginPath();
+        // ctx.strokeStyle = 'black';
+        // ctx.moveTo(0, 250);
+        // ctx.lineTo(700, 250);
+        // ctx.stroke();
 
         //initial screen position for taxi
-        window.addEventListener('DOMContentLoaded', function(e) {
-            taxii = new Crawler(40, 40, taxii.src, 20, 20);
+        // window.addEventListener('DOMContentLoaded', function(e) {
+        //     taxii = new Crawler(40, 40, img, 20, 20);
 
-            const runGame = setInterval(gameLoop, 120);
-        });
+        //     const runGame = setInterval(gameLoop, 120);
+        // });
 
-        //gameloop
-        function gameLoop() {
-            ctx.clearRect(0, 0, game.width, game.height);
-            if (taxii.alive) {
-                taxii.render();
-                console.log(taxii);
-            }
-        }
-        console.log(taxii);
-        taxii.render();
+        // ====================== SETUP FOR CANVAS RENDERING ======================= //
+        // canvas.setAttribute('height', getComputedStyle(canvas)['height']);
+        // canvas.setAttribute('width', getComputedStyle(canvas)['width']);
+
+        // const img = new Image(243, 143);
+        // img.src = 'assets/taxi.png';
+        // document.body.appendChild(img);
+
+        taxi = new Image();
+        taxi.src = 'assets/taxi.png';
+        ctx.drawImage(taxi, 350, 170);
+
+        peter = new Image();
+        peter.src = 'assets/spidey-sprite.png';
+        ctx.drawImage(peter, 90, 180);
+
+        // //constructor class
+        // class Crawler {
+        //     constructor(x, y, img, width, height) {
+        //         this.x = x;
+        //         this.y = y;
+        //         this.img = img;
+        //         this.width = width;
+        //         this.height = height;
+        //         this.alive = true;
+        //         this.render = function () {
+        //             ctx.drawImage(img, 50, 50);
+        //         }
+        //     }
+        // };
+
+        //taxii
+
+        // let taxii = new Crawler (100, 100, img, 243, 143);
+        // console.log(taxii);
+        // taxii.render();
+
+        // //gameloop
+        // //function gameLoop() {
+        //     //ctx.clearRect(0, 0, canvas.width, canvas.height);
+        //     if (taxii.alive) {
+        //         taxii.render();
+        //         console.log(taxii);
+        //     }
+        // }
+        // console.log(taxii);
+        // taxii.render();
     }
 }
 
