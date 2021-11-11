791bb39ba0c2374700b75ea84cbefe9b324224f9
pambonian <pambonian@gmail.com>
2021-11-10 21:01:45 -0500

    additional comments

--- js/sandbox.js
+++ js/sandbox.js
@@ -73,7 +73,7 @@ function start() {
     setInterval(loop, 1000/60);
 };
 
-// Gameloop Function - (?) should be the onclick? Idk. Maybe? It's possible.
+// Gameloop Function - (?) should be the onclick? Idk. Maybe? It's possible. I think.
 function gameLoop () {
 
     ctx.clearRect(0, 0, game.width, game.height);
